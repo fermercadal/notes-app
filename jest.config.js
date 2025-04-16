4c16b0ec14ce8c29f9628a4d2d456601e2faebf9
Fer Mercadal <fermercadal.1@gmail.com>
2025-04-16 20:20:51 -0300

    feat: :sparkles: Configure unit tests

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+	dir: "./",
+});
+
+const customJestConfig = {
+	testEnvironment: "jest-environment-jsdom",
+	setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+	collectCoverage: true,
+	collectCoverageFrom: [
+		"src/**/*.{ts,tsx}",
+		"!src/**/*.d.ts",
+		"!src/**/index.ts",
+		"!src/app/**/route.ts",
+	],
+	coverageDirectory: "coverage",
+	coverageReporters: ["json", "lcov", "text", "clover"],
+	moduleNameMapper: {
+		"^@/hooks/(.*)$": "<rootDir>/src/app/_hooks/$1",
+		"^@/utils/(.*)$": "<rootDir>/src/app/_utils/$1",
+		"^@/components/(.*)$": "<rootDir>/src/app/_components/$1",
+	},
+};
+
+module.exports = createJestConfig(customJestConfig);
